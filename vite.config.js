79b1f380cbf9d3dfea5bf7c2162dd790de223459
KaishuKamiya <mless.308@gmail.com>
2024-01-16 11:32:27 +0900

    :memo: test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'raku-taku',
+  base: process.env.NODE_ENV === 'production' ? '/raku-taku/' : "" ,
   plugins: [
     vue(),
   ],
